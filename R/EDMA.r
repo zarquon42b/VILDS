79dd957a4a2545946b2a14f02aadd610b9a3dfde
Stefan Schlager <zarquon42@gmail.com>
2022-05-11 13:28:38 +0200

    ILDSR2: add more output

--- R/EDMA.r
+++ R/EDMA.r
@@ -38,7 +38,13 @@ ILDS <- function(A) {
 #' @param plot logical: if TRUE show graphical output of steps involved
 #' @importFrom Morpho vecx bindArr
 #' @return
-#' matrix containing landmark information with the highest R2 (Andrea please specify here)
+#' A list containing:
+#' \item{largeR2}{containing landmark information with the highest R2 (Andrea please specify here)}
+#' \item{allR2}{vector with ILD specific R2-values, sorted decreasingly}
+#' \item{reftarILDS}{matrix with columns containing ILDs for reference and target shapes}
+#' \item{sampleILD}{matrix containing ILDs of entire sample}
+#' \item{R2tol}{R2-threshold used}
+
 #' 
 #' @examples
 #' require(Morpho)
@@ -51,9 +57,6 @@ ILDS <- function(A) {
 #' @export 
 ILDSR2 <- function(x,groups,startref,target,R2tol=.95,plot=FALSE) {
     D <- dim(x)[2] ## get LM dimensionality
-    ## convert to matrix in x1,y1,z1,... format
-    
-
     ild <- ILDS(x)
     allSILD <- round(ild, digits=6)
     if (length(dim(x)) == 3)
@@ -65,18 +68,19 @@ ILDSR2 <- function(x,groups,startref,target,R2tol=.95,plot=FALSE) {
     colnames(twosh.SILD)=c("start","target")
 
     av.twosh.SILD <- apply(twosh.SILD,1,mean);
-   if (plot) {
-        par(mfrow=c(1,3))
-        plot(twosh.SILD, asp=1)
-        plot(twosh.SILD[,1], av.twosh.SILD, asp=1, main="averaged SILDs ~ obs. SILDs?", xlab="start", ylab="averaged start-target")
-        plot(twosh.SILD[,2], av.twosh.SILD, asp=1, xlab="target", ylab="averaged start-target")
-        par(mfrow=c(1,1))
-    }
-    if (plot) {
-    if(interactive())
-        readline("proceed? (press any key to proceed)\n")
-    }
-    ratios.twosh.SILD=twosh.SILD$target/twosh.SILD$start
+    ## if (plot) {
+   ##      par(mfrow=c(1,3))
+   ##      plot(twosh.SILD, asp=1)
+   ##      plot(twosh.SILD[,1], av.twosh.SILD, asp=1, main="averaged SILDs ~ obs. SILDs?", xlab="start", ylab="averaged start-target")
+   ##      plot(twosh.SILD[,2], av.twosh.SILD, asp=1, xlab="target", ylab="averaged start-target")
+   ##      par(mfrow=c(1,1))
+   ## }
+    
+    ## if (plot) {
+    ## if(interactive())
+    ##     readline("proceed? (press any key to proceed)\n")
+    ## }
+    ratios.twosh.SILD <- twosh.SILD$target/twosh.SILD$start
     names(ratios.twosh.SILD) <- rownames(twosh.SILD)
     ratios.twosh.SILD.sorted <- sort(ratios.twosh.SILD)
     av.twosh.SILDsortedasratios <- av.twosh.SILD[names(ratios.twosh.SILD.sorted)]
@@ -87,8 +91,9 @@ ILDSR2 <- function(x,groups,startref,target,R2tol=.95,plot=FALSE) {
         plot(av.twosh.SILDsortedasratios, ratios.twosh.SILD.sorted, main="are SILD ratios varying more in smaller SILDs?", xlab="average of start & target SILD", ylab="target/start SILD ratio")
         abline(a=1, b=0, col="grey", lwd=3, lty=1) 
     }
-    all.R2=as.vector(cor(allSILD, as.numeric(groups))^2)
-    names(all.R2)=colnames(allSILD); all.R2sorted=sort(all.R2, decreasing=TRUE) # R2 of SILDs compared to factor in total sample
+    all.R2 <- as.vector(cor(allSILD, as.numeric(groups))^2)
+    names(all.R2) <- colnames(allSILD)
+    all.R2sorted <- sort(all.R2, decreasing=TRUE) # R2 of SILDs compared to factor in total sample
     av.twosh.SILDsorted <- av.twosh.SILD[names(all.R2sorted)]
     # cor(all.R2sorted, av.twosh.SILDsorted)
     if (plot) {
@@ -97,15 +102,16 @@ ILDSR2 <- function(x,groups,startref,target,R2tol=.95,plot=FALSE) {
         hist(all.R2sorted, breaks=sqrt(length(all.R2sorted)), prob=TRUE, main="hist. of target to start SILD R2s"); lines(density(all.R2sorted), col="red"); par(mfrow=c(1,1))
     }
 
-    largerR2=round(subset(all.R2sorted, all.R2sorted>stats::quantile(all.R2sorted, probs=R2tol)), digits=7)
-    
+    largerR2 <- round(subset(all.R2sorted, all.R2sorted>stats::quantile(all.R2sorted, probs=R2tol)), digits=7)
     ratios.twosh.SILD.ofBiggestR2 <- round(ratios.twosh.SILD[names(largerR2)], digits=7) # finds the corresponding SILDs ratios
-    largerR2.rankedByRatios=1+length(ratios.twosh.SILD.sorted)-rank(sort(round(abs(1-ratios.twosh.SILD.sorted), digits=7)), ties.method="random")[names(largerR2)]
-    outOf100.largerR2.rankedByRatios <- round(largerR2.rankedByRatios*100/dim(allSILD)[2], digits=0)
-    o1=rbind(largerR2, ratios.twosh.SILD.ofBiggestR2, largerR2.rankedByRatios, outOf100.largerR2.rankedByRatios)
-    o2=round(o1, digits=2)
+    largerR2.rankedByRatios <- 1+length(ratios.twosh.SILD.sorted)-rank(sort(round(abs(1-ratios.twosh.SILD.sorted), digits=7)), ties.method="random")[names(largerR2)]
+    outOf100.largerR2.rankedByRatios <- round(largerR2.rankedByRatios*100/ncol(allSILD), digits=0)
 
-    return(o2)
+
+    o1 <- rbind(largerR2, ratios.twosh.SILD.ofBiggestR2, largerR2.rankedByRatios, outOf100.largerR2.rankedByRatios)
+    o2 <- round(o1, digits=2)
+    
+    return(list(largeR2=o2,allR2=all.R2sorted,reftarILDS=twosh.SILD,sampleILD=allSILD,R2tol=R2tol))
    
 }
 
