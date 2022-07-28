605b3c2104a3da96f3dc72f7ad70f86579bc0391
Stefan Schlager <zarquon42@gmail.com>
2022-07-28 14:42:21 +0200

    Visualize: fix alpha in generic case and invert expand/contract when magnify < 1

--- R/ILDS.r
+++ R/ILDS.r
@@ -387,6 +387,8 @@ visualize.ILDSR2 <- function(x,ref=TRUE,relcol="red",rescol="black",lwd=1,cex=2,
         target <- x$reference
         x$ILDstats$reftarILDratios <- 1/x$ILDstats$reftarILDratios
     }
+    if (magnify < 0)
+        x$ILDstats$reftarILDratios <- 1/x$ILDstats$reftarILDratios
     if (magnify != 1) {
         mymean <- (reference+target)/2
         mydiff <- reference-mymean
@@ -408,7 +410,10 @@ visualize.ILDSR2 <- function(x,ref=TRUE,relcol="red",rescol="black",lwd=1,cex=2,
             mydeform(reference,target,lines=lollipop,lwd=0,show=1:2,cex2=0,cex1=cex,col1=col,pch=pch,add=add,...)
             #mydeform(ref0[-hm,,drop=FALSE],ref1[-hm,,drop=FALSE],add=T,lcol = rescol,lwd=lwd,show=1,cex2=0,cex1=0,...)
             mydeform(ref0[hm,,drop=FALSE],ref1[hm,,drop=FALSE],add=T,lcol = relcol,lwd=lwd*3,show=1,cex2=0,cex1=0,lty=1,...)
-            mydeform(ref0[-hm,,drop=FALSE],ref1[-hm,,drop=FALSE],add=T,lcol = "grey75",lwd=lwd,show=1,cex2=0,cex1=0,alpha=.5,...)
+            if (D3)
+                mydeform(ref0[-hm,,drop=FALSE],ref1[-hm,,drop=FALSE],add=T,lcol = "grey75",lwd=lwd,show=1,cex2=0,cex1=0,alpha=.5,...)
+            else
+               mydeform(ref0[-hm,,drop=FALSE],ref1[-hm,,drop=FALSE],add=T,lcol = "grey75",lwd=lwd,show=1,cex2=0,cex1=0,...) 
             
             
         }
